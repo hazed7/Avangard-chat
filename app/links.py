15dc7bb75111a89b974289a3b1a31d3776026a02
Alexander Kostin <iheartapplejr@gmail.com>
2026-04-21 02:16:07 +0300

    fix(links): raise value error on none input in linked_document_id

--- app/links.py
+++ app/links.py
@@ -6,6 +6,8 @@ from bson import DBRef
 
 
 def linked_document_id(value: Link[Any] | Document | Any) -> str:
+    if value is None:
+        raise ValueError("linked_document_id received None")
     if isinstance(value, Link):
         return str(value.ref.id)
     return str(value.id)
