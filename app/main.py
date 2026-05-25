f6feb888c9e7cdfbe2ffc1cc662dfd0f175167e6
Alexander Kostin <iheartapplejr@gmail.com>
2026-05-25 23:09:09 +0300

    feat(rooms): add last_message_at & CORS middleware

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.openapi.utils import get_openapi
 from prometheus_fastapi_instrumentator import Instrumentator
 
@@ -65,6 +66,17 @@ async def lifespan(app: FastAPI):
 app = FastAPI(
     title="Avangard API",
     lifespan=lifespan,
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 Instrumentator().instrument(app).expose(app)
